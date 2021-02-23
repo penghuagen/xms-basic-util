cc970227439179863b04e8cb60bdce62da185e67
peng.huagen <phg@4846.com>
2021-02-23 11:20:20 +0800

    增加日期格式化插件及打包脚本优化

--- build/webpack.config.js
+++ build/webpack.config.js
@@ -1,5 +1,4 @@
 const path = require('path');
-const MomentLocalesPlugin = require('moment-locales-webpack-plugin');
 module.exports = {
     mode:'production',
     entry: './src/index.js',
@@ -11,10 +10,6 @@ module.exports = {
         globalObject: 'this'
     },
     plugins: [
-        // 或者：剥离除 “en”、“es-us” 和 “ru” 以外的所有语言环境。
-        //（“en” 内置于 Moment 中，无法移除）
-        new MomentLocalesPlugin({
-            localesToKeep: ['es-us', 'ru']
-        })
+
     ]
 };
