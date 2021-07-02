42776c749549503186bfe52d5dbd31ad415a2d80
peng.huagen <phg@4846.com>
2021-07-02 20:40:30 +0800

    日期转换 小时bug修复

--- src/index.js
+++ src/index.js
@@ -1,7 +1,10 @@
-// import dateFormat from "./dateformat"; //日期格式化组件
-// import mathUtil from "./mathUtil"; //日期格式化组件
-// import funcUtil from "./funcUtil"; //日期格式化组件
+import dateFormat from "./dateformat"; //日期格式化组件
+import mathUtil from "./mathUtil"; //日期格式化组件
+import funcUtil from "./funcUtil"; //日期格式化组件
 const basicUtil = {
+    dateFormat,
+    mathUtil,
+    funcUtil
 }
 
 export default basicUtil
