645b6e328acdc32aa32ddbc1d5b83e0d7b8a981d
peng.huagen <phg@4846.com>
2021-07-02 18:11:47 +0800

    npm-learn 打包

--- src/index.js
+++ src/index.js
@@ -1,9 +1,7 @@
-import dateFormat from "./dateformat"; //日期格式化组件
-import mathUtil from "./mathUtil"; //日期格式化组件
+// import dateFormat from "./dateformat"; //日期格式化组件
+// import mathUtil from "./mathUtil"; //日期格式化组件
 // import funcUtil from "./funcUtil"; //日期格式化组件
 const basicUtil = {
-    dateFormat,
-    mathUtil
 }
 
 export default basicUtil
