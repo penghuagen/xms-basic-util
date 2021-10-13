a7bbf707e0b590c96f3674de59243a0213908416
peng.huagen <phg@4846.com>
2021-10-13 19:35:26 +0800

    学习

--- build/webpack.config.js
+++ build/webpack.config.js
@@ -9,7 +9,7 @@ module.exports = {
     entry: './src/index.js',
     output: {
         path: path.resolve(__dirname, '../dist'),
-        filename: 'xms-basic-forEach.js',
+        filename: 'xms-basic-util.js',
         library: 'xmsBasicUtil',
         libraryTarget: 'umd',
         globalObject: 'this'
